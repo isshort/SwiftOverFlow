485b7015c84729e8c09b80b83600a8a9d5f62f90
isshort <namatullahwahidi@gmail.com>
2023-01-22 08:36:42 +0600

    Initial Commit

---
+++ SwiftOverFlow/SwiftOverFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftOverFlowApp.swift
+//  SwiftOverFlow
+//
+//  Created by Nematullah on 22/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftOverFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
